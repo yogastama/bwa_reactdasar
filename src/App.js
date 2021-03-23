60b0aac081352136b99519459d1254bfa8e8a937
yogastama <yogabagas69@gmail.com>
2021-03-23 20:12:15 +0700

    belajar : - load komponen - passing props antar komponen

--- src/App.js
+++ src/App.js
@@ -2,47 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const reactjsLink = "https://reactjs.org";
-  const frameworkName = 'React JS';
-  const buttonLearnReact = <a
-    className="App-link"
-    href={reactjsLink}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn {frameworkName}
-  </a>;
-  let mulaiDetik = 1;
-  function greetingToDeveloper(name){
-    if(name){
-      return 'Hello, ' + name + '!';
-    }
-    return 'Hello Developer';
-  }
-  function nowDate(){
-    return (
-      <>
-        <p>
-          <b>Waktu saat ini : </b> { new Date().toLocaleDateString()}
-        </p>
-      </>
-    );
-  }
+  let frameworkName = "React JS";
+  let descFramework = 'Library JavaScript untuk membangun antarmuka pengguna.';
+  let linkDocs = 'https://id.reactjs.org/';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {nowDate()}
-        </p>
-        <h3>{greetingToDeveloper()}</h3>
-        {buttonLearnReact}
-      </header>
+    <div className="Apps">
+      <Header title={frameworkName} />
+      <Landing frameworkName={frameworkName} descFramework={descFramework} linkDocs={linkDocs} />
     </div>
   );
 }
-
+function Header(props){
+  let { title } = props;
+  return (
+    <div className="Header">
+      <img src={logo} className="Header-logo"/>
+      <div className="Header-title">
+        {title}
+      </div>
+    </div>
+  );
+}
+function Landing(props){
+  let {
+    frameworkName,
+    descFramework,
+    linkDocs
+  } = props;
+  return (
+      <div className="Landing">
+        <h1>
+          {frameworkName}
+        </h1>
+        <p>
+        {descFramework}
+        </p>
+        <a className="btn-docs" href={linkDocs} target="_blank">
+          Dokumentasi
+        </a>
+      </div>
+  );
+}
 export default App;
